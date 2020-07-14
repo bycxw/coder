8467e2198d5244e6545f0620235f66258813c012
bycxw <beyondchenxi@163.com>
2020-07-14 19:03:13 +0800

    zigzag level order traverse. two stacks.

--- jianzhioffer/32-1levelOrderTraverse.py
+++ jianzhioffer/32-1levelOrderTraverse.py
@@ -29,3 +29,50 @@ class Solution:
                 if node.right:
                     q.append(node.right)
         return res
+
+    def levelOrder2(self, root: TreeNode) -> List[List[int]]:
+        """Level order using a queue. BFS"""
+        if not root:
+            return []
+        q = deque([root])
+        res = []
+        while q:
+            res.append([])
+            size = len(q)
+            while size > 0:
+                size -= 1
+                node = q.popleft()
+                res[-1].append(node.val)
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+        return res
+
+    def levelOrder3(self, root: TreeNode) -> List[List[int]]:
+        """zigzag level order. Use two stacks."""
+        if not root:
+            return []
+        stacks = [[], []]
+        stacks[0].append(root)
+        current = 0
+        res = []
+        while stacks[current]:
+            size = len(stacks[current])
+            res.append([])
+            while size > 0:
+                size -= 1
+                node = stacks[current].pop()
+                res[-1].append(node.val)
+                if current:
+                    if node.right:
+                        stacks[1-current].append(node.right)
+                    if node.left:
+                        stacks[1-current].append(node.left)
+                else:
+                    if node.left:
+                        stacks[1-current].append(node.left)
+                    if node.right:
+                        stacks[1-current].append(node.right)
+            current = 1- current
+        return res
