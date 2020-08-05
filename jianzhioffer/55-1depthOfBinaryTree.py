64297401f37fa237eb23332d9f889d3a921a6829
bycxw <beyondchenxi@163.com>
2020-08-05 12:59:24 +0800

    depth of binary tree.

---
+++ jianzhioffer/55-1depthOfBinaryTree.py
@@ -0,0 +1,17 @@
+# coding: utf-8
+# https://leetcode-cn.com/problems/er-cha-shu-de-shen-du-lcof/
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+    def maxDepth(self, root: TreeNode) -> int:
+        if not root:
+            return 0
+        left_depth = self.maxDepth(root.left)
+        right_depth = self.maxDepth(root.right)
+        return max(left_depth, right_depth) + 1
