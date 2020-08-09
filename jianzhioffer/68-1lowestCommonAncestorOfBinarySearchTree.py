2e2d6f55c30510b74c19bc2f59db32ddb338d167
bycxw <beyondchenxi@163.com>
2020-08-09 18:45:22 +0800

    lowest common ancestor of binary search tree. recursion.

---
+++ jianzhioffer/68-1lowestCommonAncestorOfBinarySearchTree.py
@@ -0,0 +1,20 @@
+# coding: utf-8
+# https://leetcode-cn.com/problems/er-cha-sou-suo-shu-de-zui-jin-gong-gong-zu-xian-lcof/
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        if not root or not p or not q:
+            return None
+        if root.val > p.val and root.val > q.val:
+            return self.lowestCommonAncestor(root.left, p, q)
+        elif root.val < p.val and root.val < q.val:
+            return self.lowestCommonAncestor(root.right, p, q)
+        else:
+            return root
